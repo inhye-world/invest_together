536a9c61307efea6854c967c7652218fdc0712e1
rr_inhye <inhye1028@gmail.com>
2020-08-26 16:32:21 +0900

    version1

--- src/main/java/bit/it/into/HomeController.java
+++ src/main/java/bit/it/into/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//version1
 @Controller
 public class HomeController {
 	
